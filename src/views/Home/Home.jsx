1fbfb609255c56b60a152a2d42321d5017238763
yaogengzhu <455947455@qq.com>
2021-07-22 13:57:07 +0800

    canvas处理files文件转图片

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,28 +1,13 @@
 import React from 'react'
-import { Input, Modal, Image } from 'antd'
-import { previewImg } from '@/utils/index'
 import Banner from './modules/Banner/Bannner'
+import UploadAvatar from './modules/UploadAvatar/UploadAvatar'
 
 const Home = () => {
     return (
         <div>
             <Banner />
             <h2>图片上传</h2>
-
-            <Input
-                type="file"
-                onChange={(file) => {
-                    console.log(file.target.files, '?ss?')
-                    const files = file.target.files[0]
-                    console.log(window, '??')
-                    previewImg(files).then((res) => {
-                        Modal.info({
-                            title: '系统提示',
-                            content: <Image src={res} />
-                        })
-                    })
-                }}
-            />
+            <UploadAvatar />
         </div>
     )
 }
