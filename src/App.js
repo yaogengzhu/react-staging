d7791c6844c7a518cae66d7ab525f337b7bf5c77
yaogengzhu <455947455@qq.com>
2021-10-24 12:07:38 +0800

    refactor: 路由重构、代码异步加载

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from 'react';
 import { HashRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
-import Home from './app/Home';
-import Login from './app/Login';
+import Loadable from 'react-loadable';
+import { Loading } from 'antd-mobile';
+const Home = Loadable({
+  loader: () => import('./app/Home'),
+  loading: Loading,
+});
+const Login = Loadable({
+  loader: () => import('./app/Login'),
+  loading: Loading,
+});
 import './index';
 
 const App = () => {
