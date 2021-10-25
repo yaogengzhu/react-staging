a5dc20d0d7c071f4699f075326b269f3bcdf473a
yaogengzhu <455947455@qq.com>
2021-10-25 11:53:52 +0800

    feat: 新增自动化测试

---
+++ src/test/index.test.js
@@ -0,0 +1,20 @@
+const sum = require('./index');
+test('adds 1 + 2 to equal 3', () => {
+  expect(sum(1, 2)).toBe(3);
+});
+test('two plus two is four', () => {
+  expect(2 + 2).toBe(4);
+});
+
+test('object assignment', () => {
+  const data = { one: 1 };
+  data['two'] = 2;
+  expect(data).toEqual({ one: 1, two: 2 });
+});
+test('there is no I in team', () => {
+  expect('team').not.toMatch(/I/);
+});
+
+test('but there is a "stop" in Christoph', () => {
+  expect('Christoph').toMatch(/stop/);
+});
