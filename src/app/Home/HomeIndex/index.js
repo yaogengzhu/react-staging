cea0ac9f22fd379597dd487093308569e7788f82
yaogengzhu <455947455@qq.com>
2021-10-25 18:14:19 +0800

    feat: 主题色自定义修改、

--- src/app/Home/HomeIndex/index.js
+++ src/app/Home/HomeIndex/index.js
@@ -1,8 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { remote } from '../../../api/index';
 import './index.less';
 
 const classPrefix = 'bnq-home-index';
+
+
 const Home = () => {
+  useEffect(() => {
+    const url = remote.genDomainForEnv('auth');
+    console.log('url', url);
+  }, []);
   return (
     <div className={`${classPrefix}-page`}>
       <div className={`${classPrefix}-container`}>首页</div>
